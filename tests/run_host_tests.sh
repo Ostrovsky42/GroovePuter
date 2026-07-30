0114d2bfcbc7a77ad401a915f28f919085311df8
Ostrovsky42 <tootoo9723@gmail.com>
2026-07-30 23:51:49 +0200

    test(midi): run USB and Pattern MIDI source guards

--- tests/run_host_tests.sh
+++ tests/run_host_tests.sh
@@ -9,6 +9,7 @@ mkdir -p "${BUILD_DIR}"
 
 python3 "${ROOT_DIR}/tests/test_source_regressions.py"
 python3 "${ROOT_DIR}/tests/test_performance_source_regressions.py"
+python3 "${ROOT_DIR}/tests/test_usb_midi_source_regressions.py"
 python3 "${ROOT_DIR}/tests/test_pattern_midi_source_regressions.py"
 python3 "${ROOT_DIR}/tests/test_atlas_sound_profile.py"
 
