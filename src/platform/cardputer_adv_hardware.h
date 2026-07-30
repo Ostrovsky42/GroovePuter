70e4953c0654b57af89b93d1f6bc82aa5c6aad10
Ostrovsky42 <tootoo9723@gmail.com>
2026-07-30 12:54:03 +0200

    fix: centralize Cardputer ADV audio pins

--- src/platform/cardputer_adv_hardware.h
+++ src/platform/cardputer_adv_hardware.h
@@ -10,7 +10,18 @@
 #define GROOVEPUTER_CARDPUTER_ADV_RGB_LED_PIN (-1)
 
 namespace GroovePuterHardware {
-inline constexpr int kPowerAmplifierEnablePin = GROOVEPUTER_CARDPUTER_ADV_PA_EN_PIN;
-inline constexpr int kRgbLedDataPin = GROOVEPUTER_CARDPUTER_ADV_RGB_LED_PIN;
+inline constexpr int kPowerAmplifierEnablePin =
+    GROOVEPUTER_CARDPUTER_ADV_PA_EN_PIN;
+inline constexpr int kRgbLedDataPin =
+    GROOVEPUTER_CARDPUTER_ADV_RGB_LED_PIN;
 inline constexpr bool kRgbLedEnabled = kRgbLedDataPin >= 0;
+
+inline constexpr int kCodecI2cSdaPin = 8;
+inline constexpr int kCodecI2cSclPin = 9;
+inline constexpr uint8_t kEs8311I2cAddress = 0x18;
+
+inline constexpr int kI2sBitClockPin = 41;
+inline constexpr int kI2sWordSelectPin = 43;
+inline constexpr int kI2sDataOutPin = 42;
+inline constexpr int kI2sMasterClockPin = 0;
 }  // namespace GroovePuterHardware
