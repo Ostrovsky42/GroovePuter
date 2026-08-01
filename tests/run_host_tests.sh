cf1f0a56e60d81d74f9d8dcaba49729d13288a00
Ostrovsky42 <tootoo9723@gmail.com>
2026-08-01 19:26:41 +0200

    test: run SEQTRAK master foundation regressions

--- tests/run_host_tests.sh
+++ tests/run_host_tests.sh
@@ -265,22 +265,10 @@ python3 "${ROOT_DIR}/tests/test_atlas_sound_profile.py"
   -Wextra \
   -Werror \
   -I"${ROOT_DIR}" \
-  "${ROOT_DIR}/tests/test_usb_midi_smf_output.cpp" \
-  "${ROOT_DIR}/src/midi/usb_midi_output.cpp" \
-  -o "${BUILD_DIR}/test_usb_midi_smf_output"
+  "${ROOT_DIR}/tests/test_transport_clock_source.cpp" \
+  -o "${BUILD_DIR}/test_transport_clock_source"
 
-"${BUILD_DIR}/test_usb_midi_smf_output"
-
-"${CXX}" \
-  -std=c++17 \
-  -Wall \
-  -Wextra \
-  -Werror \
-  -I"${ROOT_DIR}" \
-  "${ROOT_DIR}/tests/test_musical_event_queue.cpp" \
-  -o "${BUILD_DIR}/test_musical_event_queue"
-
-"${BUILD_DIR}/test_musical_event_queue"
+"${BUILD_DIR}/test_transport_clock_source"
 
 "${CXX}" \
   -std=c++17 \
@@ -288,49 +276,18 @@ python3 "${ROOT_DIR}/tests/test_atlas_sound_profile.py"
   -Wextra \
   -Werror \
   -I"${ROOT_DIR}" \
-  "${ROOT_DIR}/tests/test_sampler_voice.cpp" \
-  "${ROOT_DIR}/src/sampler/sampler_voice.cpp" \
-  -o "${BUILD_DIR}/test_sampler_voice"
+  "${ROOT_DIR}/tests/test_external_midi_transport_event_queue.cpp" \
+  -o "${BUILD_DIR}/test_external_midi_transport_event_queue"
 
-"${BUILD_DIR}/test_sampler_voice"
+"${BUILD_DIR}/test_external_midi_transport_event_queue"
 
 "${CXX}" \
   -std=c++17 \
   -Wall \
   -Wextra \
   -Werror \
-  -pthread \
   -I"${ROOT_DIR}" \
-  "${ROOT_DIR}/tests/test_audio_mutation_gate.cpp" \
-  -o "${BUILD_DIR}/test_audio_mutation_gate"
+  "${ROOT_DIR}/tests/test_external_midi_clock_tracker.cpp" \
+  -o "${BUILD_DIR}/test_external_midi_clock_tracker"
 
-"${BUILD_DIR}/test_audio_mutation_gate"
-
-"${CXX}" \
-  -std=c++17 \
-  -Wall \
-  -Wextra \
-  -I"${ROOT_DIR}" \
-  -I"${ROOT_DIR}/platform_sdl" \
-  -include "${ROOT_DIR}/platform_sdl/arduino_compat.h" \
-  "${ROOT_DIR}/tests/test_scene_roundtrip.cpp" \
-  "${ROOT_DIR}/scenes.cpp" \
-  "${ROOT_DIR}/json_evented.cpp" \
-  "${ROOT_DIR}/src/audio/pattern_paging.cpp" \
-  -o "${BUILD_DIR}/test_scene_roundtrip"
-
-"${BUILD_DIR}/test_scene_roundtrip"
-
-"${CXX}" \
-  -std=c++17 \
-  -Wall \
-  -Wextra \
-  -I"${ROOT_DIR}" \
-  -I"${ROOT_DIR}/platform_sdl" \
-  -include "${ROOT_DIR}/platform_sdl/arduino_compat.h" \
-  "${ROOT_DIR}/tests/test_pattern_paging.cpp" \
-  "${ROOT_DIR}/src/audio/pattern_paging.cpp" \
-  -o "${BUILD_DIR}/test_pattern_paging"
-
-"${BUILD_DIR}/test_pattern_paging"
-echo "host regressions: OK"
+"${BUILD_DIR}/test_external_midi_clock_tracker"
