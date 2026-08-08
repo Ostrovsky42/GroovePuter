6ee915f75b09ed270c92035b7d0bd9c7078516e6
Ostrovsky42 <tootoo9723@gmail.com>
2026-08-08 14:03:36 +0200

    test: run Stage 2 adversarial matrix

--- tests/run_rhythm_stage2_tests.sh
+++ tests/run_rhythm_stage2_tests.sh
@@ -7,8 +7,7 @@ mkdir -p "${BUILD_DIR}"
 
 python3 "${ROOT_DIR}/tests/test_rhythm_stage2_source_regressions.py"
 
-SOURCES=(
-  "${ROOT_DIR}/tests/test_rhythm_stage2.cpp"
+COMMON_SOURCES=(
   "${ROOT_DIR}/src/generation/generation_context.cpp"
   "${ROOT_DIR}/src/generation/rhythm/rhythm_catalog.cpp"
   "${ROOT_DIR}/src/generation/rhythm/relationship_resolver.cpp"
@@ -17,20 +16,33 @@ SOURCES=(
 
 build_and_run() {
   local compiler="$1"
-  local output="$2"
-  shift 2
+  local test_source="$2"
+  local output="$3"
+  shift 3
   "${compiler}" -std=c++17 -Wall -Wextra -Werror -I"${ROOT_DIR}" \
-    "$@" "${SOURCES[@]}" -o "${output}"
+    "$@" "${test_source}" "${COMMON_SOURCES[@]}" -o "${output}"
   "${output}"
 }
 
-build_and_run "${CXX:-g++}" "${BUILD_DIR}/test_rhythm_stage2_gcc"
+run_suite() {
+  local suffix="$1"
+  local compiler="$2"
+  shift 2
+  build_and_run "${compiler}" \
+    "${ROOT_DIR}/tests/test_rhythm_stage2.cpp" \
+    "${BUILD_DIR}/test_rhythm_stage2_${suffix}" "$@"
+  build_and_run "${compiler}" \
+    "${ROOT_DIR}/tests/test_rhythm_stage2_adversarial.cpp" \
+    "${BUILD_DIR}/test_rhythm_stage2_adversarial_${suffix}" "$@"
+}
+
+run_suite gcc "${CXX:-g++}"
 
 if command -v clang++ >/dev/null 2>&1; then
-  build_and_run clang++ "${BUILD_DIR}/test_rhythm_stage2_clang"
+  run_suite clang clang++
 fi
 
-build_and_run "${CXX:-g++}" "${BUILD_DIR}/test_rhythm_stage2_sanitize" \
+run_suite sanitize "${CXX:-g++}" \
   -O1 -g -fno-omit-frame-pointer -fsanitize=address,undefined
 
 printf 'Groove Vocabulary Stage 2 host matrix: OK\n'
