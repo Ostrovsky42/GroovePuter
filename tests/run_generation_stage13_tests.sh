5f1a4385ceccc5e0de2688bd0cc138f68ad4d7b5
Ostrovsky42 <tootoo9723@gmail.com>
2026-08-10 21:51:02 +0200

    tests: wire Stage 14.1 source guard

--- tests/run_generation_stage13_tests.sh
+++ tests/run_generation_stage13_tests.sh
@@ -9,6 +9,7 @@ python3 "${ROOT_DIR}/tests/test_generation_stage13_source_regressions.py"
 python3 "${ROOT_DIR}/tests/test_generation_orthogonality_source_regressions.py"
 python3 "${ROOT_DIR}/tests/test_generation_stage14_source_regressions.py"
 python3 "${ROOT_DIR}/tests/test_generation_stage14_drum_generate_source_regressions.py"
+python3 "${ROOT_DIR}/tests/test_generation_stage14_p_level_source_regressions.py"
 
 COMPOSITION_SOURCES=(
   "${ROOT_DIR}/src/generation/generation_context.cpp"
