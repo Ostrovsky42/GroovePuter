e9312749132f4b002deb00d5c4c1f14f073308dd
Ostrovsky42 <tootoo9723@gmail.com>
2026-08-10 23:12:12 +0200

    tests: run all-genre audition selection coverage

--- tests/run_generation_stage13_tests.sh
+++ tests/run_generation_stage13_tests.sh
@@ -71,6 +71,9 @@ run_suite() {
   build_and_run "${compiler}" \
     "${ROOT_DIR}/tests/test_generation_stage14_p_level_semantics.cpp" \
     "${BUILD_DIR}/test_generation_stage14_p_levels_${suffix}" migration "$@"
+  build_and_run "${compiler}" \
+    "${ROOT_DIR}/tests/test_stage12_audition_genre_coverage.cpp" \
+    "${BUILD_DIR}/test_stage12_audition_genres_${suffix}" migration "$@"
 }
 
 run_suite gcc "${CXX:-g++}"
