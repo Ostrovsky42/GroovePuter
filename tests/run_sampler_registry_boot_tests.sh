5d8181b14b83c97ec096e24044e255b1dd578be8
Ostrovsky42 <tootoo9723@gmail.com>
2026-08-14 19:56:03 +0200

    0.9.3-D: link sampler persistence identity contract

--- tests/run_sampler_registry_boot_tests.sh
+++ tests/run_sampler_registry_boot_tests.sh
@@ -15,6 +15,7 @@ mkdir -p "${BUILD_DIR}"
   -I"${ROOT_DIR}" \
   "${ROOT_DIR}/tests/test_sample_registry_boot.cpp" \
   "${ROOT_DIR}/src/sampler/sample_index.cpp" \
+  "${ROOT_DIR}/src/sampler/sample_scene_persistence.cpp" \
   "${ROOT_DIR}/src/sampler/ram_sample_store.cpp" \
   -o "${BUILD_DIR}/test_sample_registry_boot"
 
