bf079df135b0f13b8af0397642dace7585743716
Ostrovsky42 <tootoo9723@gmail.com>
2026-09-04 23:03:26 -0700

    test(p3-u1): execute bounded phrase mutation characterization

--- tests/run_pattern_phrase_p3_u1_tests.sh
+++ tests/run_pattern_phrase_p3_u1_tests.sh
@@ -8,8 +8,13 @@ python3 tests/test_p3_u1_source_projection.py
 
 FAST_BUILD_DIR="${ROOT_DIR}/build/pattern-phrase-p3-u1-fast"
 mkdir -p "${FAST_BUILD_DIR}"
+
 g++ -std=c++17 -I. tests/test_p3_u1_phrase_projection.cpp \
   -o "${FAST_BUILD_DIR}/test_p3_u1_phrase_projection"
 "${FAST_BUILD_DIR}/test_p3_u1_phrase_projection"
+
+g++ -std=c++17 -I. tests/test_p3_u1_phrase_mutation.cpp \
+  -o "${FAST_BUILD_DIR}/test_p3_u1_phrase_mutation"
+"${FAST_BUILD_DIR}/test_p3_u1_phrase_mutation"
 
 bash tests/run_pattern_phrase_p3_tests.sh
