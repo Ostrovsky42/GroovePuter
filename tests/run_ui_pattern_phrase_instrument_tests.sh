3757ba2e975d24a31f391785c82bcf3b9a1fd114
Ostrovsky42 <tootoo9723@gmail.com>
2026-09-08 06:25:15 -0700

    test(ui): gate roll selection causality

--- tests/run_ui_pattern_phrase_instrument_tests.sh
+++ tests/run_ui_pattern_phrase_instrument_tests.sh
@@ -13,6 +13,7 @@ g++ -std=c++17 -I. tests/test_ui_phrase_selected_event_edits.cpp \
   -o "${BUILD_DIR}/test_ui_phrase_selected_event_edits"
 "${BUILD_DIR}/test_ui_phrase_selected_event_edits"
 
+python3 tests/test_ui_phrase_roll_selection_causality.py
 python3 tests/test_ui_pattern_phrase_instrument_integration.py
 
 # Existing focused causal contracts that this checkpoint must preserve.
