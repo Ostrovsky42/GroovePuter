f15aea51be11e1d03c0591c2cd53059becf896c0
Ostrovsky42 <tootoo9723@gmail.com>
2026-09-09 14:54:32 -0700

    test(gf2): replay baseline C0R5 observer on I6 production

---
+++ tools/gf2/g4_c0r4_bass_candidates_probe.h
@@ -0,0 +1,17 @@
+#ifndef GROOVEPUTER_TOOLS_GF2_G4_C0R4_BASS_CANDIDATES_PROBE_H
+#define GROOVEPUTER_TOOLS_GF2_G4_C0R4_BASS_CANDIDATES_PROBE_H
+
+#include "../../src/generation/roles/bass_rhythm.h"
+#include "../../src/generation/rhythm/rhythm_types.h"
+
+#include <cstdint>
+
+namespace GroovePuterRhythm {
+namespace G4C0R4 {
+
+uint16_t nativeBassCandidateMask(RhythmFamily family);
+
+}  // namespace G4C0R4
+}  // namespace GroovePuterRhythm
+
+#endif  // GROOVEPUTER_TOOLS_GF2_G4_C0R4_BASS_CANDIDATES_PROBE_H
