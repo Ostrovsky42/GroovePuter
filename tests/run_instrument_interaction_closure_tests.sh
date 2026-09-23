dfaf25ba868858707743b9d40762ed6867778f07
Ostrovsky42 <tootoo9723@gmail.com>
2026-09-23 13:50:46 -0700

    test(ui): match Pattern host compiler compatibility flags

--- tests/run_instrument_interaction_closure_tests.sh
+++ tests/run_instrument_interaction_closure_tests.sh
@@ -19,7 +19,7 @@ python3 tests/test_hub_song_drum_ui_source_regressions.py
   -o "${BUILD_DIR}/test_cardputer_input_edges"
 "${BUILD_DIR}/test_cardputer_input_edges"
 
-"${CXX}" -std=c++17 -Wall -Wextra -Werror -I"${ROOT_DIR}" \
+"${CXX}" -std=c++17 -Wall -Wextra -Werror -Wno-c++20-extensions -I"${ROOT_DIR}" \
   tests/test_pattern_mutations_0_9_8_r3.cpp \
   -o "${BUILD_DIR}/test_pattern_mutations"
 "${BUILD_DIR}/test_pattern_mutations"
